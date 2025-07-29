164cc10f396c9139c83b7168919f5fc452f1579e
Jue0000 <tzaw5266@gmail.com>
2025-07-29 16:22:54 +0630

    Insert Product

--- admin/insertProduct.php
+++ admin/insertProduct.php
@@ -9,13 +9,45 @@
  }catch(PDOException $e){
     echo $e->getMessage();
  }
+
+
+ if (isset($_POST['insertBtn'])) 
+    {
+$name=$_POST['name'];
+$price=$_POST['price'];
+$category=$_POST['category'];
+$qty=$_POST['qty'];
+$description=$_POST['description'];
+$fileImage=$_FILES['productImage'];
+// echo $name. "<br>";
+// echo $price."<br>";
+// echo $category."<br>";
+// echo $qty."<br>";
+// echo $description."<br>";
+// echo $fileImage['name']."<br>";
+// echo "$fileImage[size]<br>";
+// echo "$fileImage[type]<br>";
+
+$filePath="productImage/".$fileImage['name'];
+$status=move_uploaded_file($fileImage['tmp_name'],$filePath);
+
+if($status){
+echo "File uploaded";
+echo "<img src=$filePath>";
+} else
+{
+    echo "file upload fail";
+}
+    }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Insert Product</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
@@ -25,23 +57,19 @@
 <div class="container-fluid">
     <div class="row">
         <?php require_once("navbarcopy.php");?>
-
-
-
-    </div>
-    <div class="row mx-auto">
-        <div class="col-md-9"></div>
-
-            <form class="form" action="insertProduct.php" method="post" enctype="multipart/form-data">
-                <div class="col-md-4">
+  </div>
+        <div class="row mt-3 mx-auto">
+    <div class="col-md-12 mx-auto">
+<h4>Insert Product</h4>
+            <form class="form border border-dark rounded p-4" action="insertProduct.php" method="post" enctype="multipart/form-data">
+                <div class="row px-5">
+                <div class="col-md-5 mx-3 px-5">
                     <div class="mb-1">
-
                     <label for="pname" class="form-label">Product Name</label>
-                    <input type="text" class="form-control" name="pname">
+                    <input type="text" class="form-control" name="name">
                 </div>
 
                     <div class="mb-1">
-
                     <label for="price" class="form-label">Price</label>
                     <input type="number" class="form-control" name="price">
                 </div>
@@ -57,16 +85,36 @@
                 }
                     ?>
                 </select>
+                </div>
 
+                <div class="col-md-5 mx-3 px-5">
+                    <div class="mb-1">
+                    <label class="form-label">Quantity</label>
+                    <input type="number" class="form-control" name="qty">
+                </div>
+
+                    <div class="mb-1">
+                        <textarea name="description" id="" class="form-control">
+
+                        </textarea>
+                </div>
+                <div class="mb-1">
+                    <label for="" class="form-label">Choose Product Image</label>
+                    <input type="file" class="form-control" name="productImage">
+            </div>
+            <button type="submit" name="insertBtn"class="btn btn-primary rounded-pill mt-3">Insert Product</button>
+                </div>
 
                 </div>
-            </div>
+               
+         
 
             </form>
-        
-
+       </div>
+ 
+ </div>
     </div>
-</div>
+
     
 </body>
 </html>
