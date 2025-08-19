3135a0fc192b86e090502f85d62809ed80afcf71
Jue0000 <tzaw5266@gmail.com>
2025-08-19 15:51:15 +0630

    radio filter and category filter is added

--- admin/editDelete.php
+++ admin/editDelete.php
@@ -1,30 +1,194 @@
 <?php
 require_once "dbconnect.php";
-if(!isset($_SESSION))
-{
-session_start();
+if (!isset($_SESSION)) {
+    session_start();
 }
-if(isset($_GET['eid']))
-{
-echo "edit button clicked";
-
-}else if(isset($_GET["did"]))
-{
-try{
-    $productID= $_GET["did"];
-$sql="delete from product where productID=?";
-$stmt=$conn->prepare($sql);
-$status=$stmt->execute([$productID]);
-if($status)
-{
-    $_SESSION['deleteSuccess']="product ID $productID has been deleted.";
-    header("Location:viewProduct.php");
-}
-} catch(PDOException $e) {
+try {
+    $sql = "select * from category";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $categories = $stmt->fetchAll();
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
+if (isset($_GET['eid'])) { // true when edit button is clicked
+    $productID = $_GET['eid'];
+    try {
+        $sql = "SELECT p.productID,p.productName,
+c.catName, p.category,
+p.price, p.description,
+p.qty, p.imgPath
+from product p, category c
+where p.category =c.catID AND
+p.productID=?";
+
+        $statement = $conn->prepare($sql);
+        $statement->execute([$productID]);
+        $product = $statement->fetch();
+        $_SESSION['product'] = $product;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+} else if (isset($_GET["did"])) {
+    try {
+        $productID = $_GET["ditd"];
+        $sql = "delete from product where productID=?";
+        $stmt = $conn->prepare($sql);
+        $status = $stmt->execute([$productID]);
+        if ($status) {
+            $_SESSION['deleteSuccess'] = "product ID $productID has been deleted.";
+            header("Location:viewProduct.php");
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+else if (isset ($_POST['updateBtn']))
+{ $productName =$_POST['pname'];
+    $category=$_POST['category'];
+    $price=$_POST['price'];
+    $description=$_POST['description'];
+    $qty=$_POST['qty'];
+    $fileImg=$_FILES['file'];//size, type, name, tmp_name
+
+    //specify directory or folder to store product image 
+    $filePath="productImage/$fileImg[name]";
+    $status=move_uploaded_file($fileImg['tmp_name'],$filePath);
+
+    if ($status == true)
+    {
+        try{ $pid=$_POST['pid'];
+            // DML data manipulate language, update, delete, insert
+            $sql="update product set productName=?, category=?, price=?, qty=?, description=?, imgPath=? where productID=?";
+            $stmt=$conn->prepare($sql);
+            $status=$stmt->execute([$productName, $category, $price,$qty,$description,$filePath, $pid]);
+
+            if($status)
+            {$_SESSION['updateMessage']="Product with product id $pid is updated!!!";
+                header("Location:viewProduct.php");
+            }
+
+        }catch(PDOException $e)
+        {
+            echo $e-> getMessage();
+        }
+    }
 
 }
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <?php require_once "navbarcopy.php"; ?>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+
+            </div>
+            <div class="col-md-10">
+                
+                    
+                        <form action="editDelete.php" class="form card p-4" method="post" enctype="multipart/form-data">
+                            <input type="hidden" name="pid" value="<?php echo $product['productID']; ?>">
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="pname" class="form-label">Product Name</label>
+                                <input type="text" class="form-control" name="pname" id="pname"
+                                    value="<?php if (isset($product)) {
+                                                echo $product['productName'];
+                                            }
+                                            ?>">
+
+
+                            </div>
+
+                            <div class="mb-3">
+                                <p class="alert alert-info"><?php echo "Previous selected category $product[catName]"; ?></p>
+                                <select name="category" id="category" class="form-select">
+                                    <option value="0">Choose Category</option>
+                                    <?php
+                                    if (isset($categories)) {
+                                        foreach ($categories as $category) {
+                                            echo "<option value=$category[catId]> $category[catName] </option>";
+                                        }
+                                    }
+                                    ?>
+
+                                </select>
+
+                            </div>
+                            <div class="mb-2">
+                                <label for="price" class="form-label">Price</label>
+                                <input type="number" id="price"class="form-control" name="price" value="<?php
+                                            if (isset($product)) {
+                                                echo $product['price'];
+                                            } ?>">
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="mb-2">
+                                <label for="desc" class="form-label">Description</label>
+                                <textarea name="description" id="desc" class="form-control" placeholder="write description here">
+
+                                </textarea>
+                            </div>
+                            <div class="mb-2">
+                                <label for="qty" class="form-label">Quantity</label>
+                                <input type="number" class="form-control" name="qty" id="qty"value="<?php
+                                            if (isset($product)) {
+                                                echo $product['qty'];
+                                            } ?>">
+                            </div>
+                            <div class="mb-2">
+                                <?php
+                                            if (isset($product)) {
+                                               echo "<img class='img-responsive' style=width:100px; height:100px; src=$product[imgPath]>";
+                                            } ?>
+                                <label for="img" class="form-label">Product Image</label>
+                                <input type="file" class="form-control" name="file" id="img">
+                            </div>
+                            <div class="mb-2">
+                                <button type="submit" class="btn btn-primary" name="updateBtn">Update</button>
+                            </div>
+
+                        </div>
+
+
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+    </div>
+
+
+
+
+</body>
+
+</html>
--- admin/viewProduct.php
+++ admin/viewProduct.php
@@ -3,8 +3,20 @@ if (!isset($_SESSION)) {
     session_start();
 }
 require_once "dbconnect.php";
+
+ try{
+    $sql="select * from category";
+  $stmt=$conn->prepare($sql);
+  $stmt->execute();
+  $categories=$stmt->fetchAll();
+
+ }catch(PDOException $e){
+    echo $e->getMessage();
+ }
+
 try {
-    $sql = "SELECT p.productID, p.productName, p.price, p.description, p.qty, p.imgPath, c.catname as category
+    $sql = "SELECT p.productID, p.productName, p.price, 
+    p.description, p.qty, p.imgPath, c.catname as category
     FROM product p, category c WHERE
     p.category = c.catId";
    $stmt = $conn->prepare($sql);
@@ -15,6 +27,74 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage();
 }
+if(isset($_GET['bsearch']))
+{
+    $text=$_GET['tsearch'];
+    try{
+        $sql="SELECT p.productID, p.productName, p.price,
+         p.description, p.qty, p.imgPath, c.catname as category
+    FROM product p, category c WHERE
+    p.category = c.catId and
+    p.productName like ?";
+   $stmt= $conn->prepare($sql);
+   $stmt->execute(["%".$text."%"]);
+   $products=$stmt->fetchAll();
+
+    }catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+}
+else if(isset($_GET['cSearch']))
+{
+    $cid=$_GET['category'];
+     try{
+        $sql="SELECT p.productID, p.productName, p.price,
+         p.description, p.qty, p.imgPath, c.catname as category
+    FROM product p, category c WHERE
+    p.category = c.catId and
+    c.catId=?";
+   $stmt= $conn->prepare($sql);
+   $stmt->execute([$cid]);
+   $products=$stmt->fetchAll();
+
+    }catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+}
+else if(isset($_POST['radioBtn'])){//radio search
+    $price=$_POST['price'];
+    if($price=="first")
+    {
+        $lower=200;
+        $upper=300;
+    }
+    else if($price=="second"){
+        $lower=301;
+        $upper=500;
+    }
+    
+    try{
+        $sql="select p.productID,p.productName,p.price,p.qty,
+        p.description,p.imgPath, c.catName as category
+        from product p, category c 
+        where p.price BETWEEN ? and ?
+        AND c.catId=p.category;";
+
+        $stmt=$conn->prepare($sql);
+        $stmt->execute([$lower,$upper]);
+        $products=$stmt->fetchAll();
+
+
+
+    }catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+}
+
+
 
 ?>
 
@@ -36,7 +116,46 @@ try {
         </div>
         <div class="row"><!-- content    -->
             <div class="col-md-2 py-5">
-                <a href="insertprouct.php" class="btn btn-outline-primary">New Product</a>
+                <div class="card">
+                <a href="insertprouct.php" class="btn btn-outline-primary card-link">New Product</a>
+            </div>
+            <div class="card">
+                <div class="card-title">Category Search</div>
+                <div class="card-body">
+                    <form class="form" method="get" action="viewProduct.php">
+                    <select name="category" id="" class="form-select mb-2">
+                        <?php
+                        foreach($categories as $category)
+                        {
+                            echo "<option value=$category[catId]>$category[catName]</option>";
+                        }
+
+
+                        ?>
+
+                    </select>
+                <button name="cSearch" type="submit" class="btn btn-outline-primary rounded-pill">Search</button>
+                    </form>
+                </div>
+            </div>
+            <div class="card mb-3">
+                <form class="form" action="viewProduct.php" method="post">
+                <div class="form-check">
+                        <input type="radio" name="price" value="first" class="form-check-input">
+                        <label for="" class="form-check-label">$200-$300</label>
+                </div>
+
+                <div class="form-check mb-2">
+                        <input type="radio" name="price" value="second" class="form-check-input">
+                        <label for="" class="form-check-label">$301-$400</label>
+                </div>
+                <div class="mb-2">
+                    <button name="radioBtn" class="btn btn-outline-primary rounded-pill">Search</button>
+                </div>
+
+
+                        </form>
+            </div>
             </div>
             <div class="col-md-10 py-5"><!-- content    -->
                 <?php
@@ -47,6 +166,11 @@ try {
                 else if (isset($_SESSION['deleteSuccess'])) {
                     echo "<p class='alert alert-success'>$_SESSION[deleteSuccess] </p>";
                     unset($_SESSION['deleteSuccess']);
+                }
+                else if(isset($_SESSION['updateMessage']))
+                {
+                    echo "<p class='alert alert-success'> $_SESSION[updateMessage] </p>";
+                    unset($_SESSION['updateMessage']);
                 }
                 ?>
                 <table class="table table-striped">
